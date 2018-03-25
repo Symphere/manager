d9aa99a7e451202ec429c591869a3ab3faca5401
symphere <791768388@qq.com>
2018-03-25 21:04:33 +0800

    1.出差表相关操作添加。 2.目前全域开放。 3.修改命名方式。

--- src/main/java/com/cetc/manager/controller/GenderController.java
+++ src/main/java/com/cetc/manager/controller/GenderController.java
@@ -4,13 +4,14 @@ package com.cetc.manager.controller;
 import com.cetc.manager.dao.GenderDao;
 import com.cetc.manager.entity.Gender;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/Gender")
+@RequestMapping("/gender")
 public class GenderController {
     @Autowired
     GenderDao genderDao;
--- src/main/java/com/cetc/manager/dao/BusinessTripDao.java
+++ src/main/java/com/cetc/manager/dao/BusinessTripDao.java
@@ -1,7 +1,19 @@
 package com.cetc.manager.dao;
 
 import com.cetc.manager.entity.BusinessTrip;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface BusinessTripDao extends JpaRepository<BusinessTrip, String> {
+
+    List<BusinessTrip> findByJobNumber(String jobNumber);
+
+
+    @Query(value = "select * from business_trip where id=?1", nativeQuery = true)
+    BusinessTrip findWithId(String id);
 }
