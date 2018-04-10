ff471e0ab9ff7fac0cd9f008b0bec5f2b25e2ea0
symphere <791768388@qq.com>
2018-04-10 20:47:29 +0800

    查询没有结果时返回 not found

--- src/main/java/com/cetc/manager/controller/WorkLogController.java
+++ src/main/java/com/cetc/manager/controller/WorkLogController.java
@@ -4,6 +4,7 @@ import com.cetc.manager.common.Const;
 import com.cetc.manager.common.Mapping;
 import com.cetc.manager.common.MyUUID;
 import com.cetc.manager.entity.WorkLog;
+import com.cetc.manager.entity.WorkLogVO;
 import com.cetc.manager.service.WorkLogService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,13 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/workLog")
 public class WorkLogController {
 
-    @Resource(name="workLogService")
+    @Resource(name = "workLogService")
     WorkLogService workLogService;
 
     @RequestMapping("/add")
@@ -26,13 +28,13 @@ public class WorkLogController {
                                    @RequestParam("jobNumber") String jobNumber,
                                    @RequestParam("fillingTime") Timestamp fillingTime, // 记录时间
                                    @RequestParam("startTime") Date startTime, // 开始时间
-                                   @RequestParam("endTime")  Date endTime,  // 本周结束时间
+                                   @RequestParam("endTime") Date endTime,  // 本周结束时间
                                    @RequestParam("workContent") String workContent, // 本周工作
                                    @RequestParam("workPlan") String workPlan, // 下周计划
-                                   @RequestParam(value = "remark",required = false, defaultValue = "") String remark, // 备注
-                                   @RequestParam(value = "coordinate", required = false, defaultValue = "") String coordinate ){
+                                   @RequestParam(value = "remark", required = false, defaultValue = "") String remark, // 备注
+                                   @RequestParam(value = "coordinate", required = false, defaultValue = "") String coordinate) {
         WorkLog workLog = new WorkLog();
-        workLog.setId(id.equals("")?MyUUID.getUUID():id);
+        workLog.setId(id.equals("") ? MyUUID.getUUID() : id);
         workLog.setJobNumber(jobNumber);
         workLog.setFillingTime(fillingTime);
         workLog.setStartTime(startTime);
@@ -41,40 +43,56 @@ public class WorkLogController {
         workLog.setWorkPlan(workPlan);
         workLog.setRemark(remark);
         workLog.setCoordinate(coordinate);
-        if(workLogService.saveAndFlush(workLog)){
-            return Mapping.map(0,"success",true);
-        } else{
-            return Mapping.map(1,"failure",false);
+        if (workLogService.saveAndFlush(workLog)) {
+            return Mapping.map(0, "success", true);
+        } else {
+            return Mapping.map(1, "failure", false);
         }
     }
 
     @RequestMapping("/findByJobNumber")
-    public Map<String, Object> findByJobNumber(@RequestParam("jobNumber") String jobNumber){
-        System.out.println("jobNumber: "+jobNumber);
-        return Mapping.map(0,"success",workLogService.findByJobNumber(jobNumber));
+    public Map<String, Object> findByJobNumber(@RequestParam("jobNumber") String jobNumber) {
+        System.out.println("jobNumber: " + jobNumber);
+        List<WorkLogVO> workLogVOS = workLogService.findByJobNumber(jobNumber);
+        if (workLogVOS.isEmpty()) {
+            return Mapping.map(1, "not found", null);
+        }
+        return Mapping.map(0, "success", workLogVOS);
     }
 
     @RequestMapping("/findById")
-    public Map<String, Object> findById(@RequestParam("id")String id){
-        return Mapping.map(0,"success",workLogService.findWithId(id));
+    public Map<String, Object> findById(@RequestParam("id") String id) {
+        WorkLogVO workLogVO = workLogService.findWithId(id);
+        if (workLogVO == null) {
+            return Mapping.map(1, "not found", null);
+        }
+        return Mapping.map(0, "success", workLogVO);
     }
 
     @RequestMapping("/all")
-    public Map<String, Object> findAll(){
-        return Mapping.map(0,"success",workLogService.findAll());
+    public Map<String, Object> findAll() {
+        List<WorkLogVO> workLogVOS = workLogService.findAll();
+        if (workLogVOS.isEmpty()) {
+            return Mapping.map(1, "not found", null);
+        }
+        return Mapping.map(0, "success", workLogVOS);
     }
 
     @RequestMapping("/search")
-    public Map<String, Object> search(@RequestParam(value="name",defaultValue = "") String name,
-                                      @RequestParam(value = "startTime",defaultValue = "1970-01-01")Date startTime,
-                                      @RequestParam(value = "endTime",defaultValue = "1970-01-01")Date endTime){
+    public Map<String, Object> search(@RequestParam(value = "name", defaultValue = "") String name,
+                                      @RequestParam(value = "startTime", defaultValue = "1970-01-01") Date startTime,
+                                      @RequestParam(value = "endTime", defaultValue = "1970-01-01") Date endTime) {
 //        return Mapping.map("0", "查询成功", businessTripServices.search(name, destination, startTime, endTime, approvalName));
-        if(Const.DEFAULT_DATE.equals(startTime)){
+        if (Const.DEFAULT_DATE.equals(startTime)) {
             startTime = null;
         }
-        if(Const.DEFAULT_DATE.equals(endTime)){
+        if (Const.DEFAULT_DATE.equals(endTime)) {
             endTime = null;
         }
-        return Mapping.map(0, "success",workLogService.search(name, startTime, endTime));
+        List<WorkLogVO> workLogVOS = workLogService.search(name, startTime, endTime);
+        if (workLogVOS.isEmpty()) {
+            return Mapping.map(1, "not found", null);
+        }
+        return Mapping.map(0, "success", workLogVOS);
     }
 }
