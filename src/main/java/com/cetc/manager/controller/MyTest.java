b6f75d7d225e1644f5b24cc64a118be3a1b45055
symphere <791768388@qq.com>
2018-04-22 12:25:28 +0800

    添加几个依赖

--- src/main/java/com/cetc/manager/controller/MyTest.java
+++ src/main/java/com/cetc/manager/controller/MyTest.java
@@ -1,15 +1,23 @@
 package com.cetc.manager.controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 @RequestMapping("/test")
 public class MyTest {
 
     @RequestMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @RequestMapping("/test")
+    public ModelAndView test(){
+        ModelAndView modelAndView = new ModelAndView("index2");
+        return modelAndView;
     }
 }
